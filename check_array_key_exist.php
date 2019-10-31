fdfcf97a3a72f43fb280859128639eda0003e661
asanandan <anandanhere@gmail.com>
2019-10-31 11:38:48 +0530

    Create check_array_key_exist.php
    It's simple example to check whether the corresponding key name exist or not in the array

---
+++ check_array_key_exist.php
@@ -0,0 +1,13 @@
+//It's simple example to check whether the corresponding key name exist or not in the array
+
+<?php
+$a=array("Volvo"=>"XC90","BMW"=>"X5");
+if (array_key_exists("Volvo",$a))
+  {
+  echo "Key exists!";
+  }
+else
+  {
+  echo "Key does not exist!";
+  }
+?>
